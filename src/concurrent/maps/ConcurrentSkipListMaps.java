48f5f02140e93a41e950c1e3a1a4ce87366aec6d
Choudhary Shekhar <shekhar.choudhary@tieto.com>
2018-07-31 19:27:14 +0530

    adding changes

--- src/concurrent/maps/ConcurrentSkipListMaps.java
+++ src/concurrent/maps/ConcurrentSkipListMaps.java
@@ -1,5 +1,23 @@
 package concurrent.maps;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentSkipListMap;
+
 public class ConcurrentSkipListMaps {
 
+	public static void main(String[] args) {
+		Map<Integer, String> concurrentSkipListMap = new ConcurrentSkipListMap<>();
+		concurrentSkipListMap.put(1, "one");
+		concurrentSkipListMap.put(3, "three");
+		concurrentSkipListMap.put(2, "two");
+		concurrentSkipListMap.put(4, "four");
+		concurrentSkipListMap.put(6, "six");
+		concurrentSkipListMap.put(5, "five");
+		
+		
+		
+		
+		
+	}
 }
